6fa04619f91d6b2f183e1c7dcc44408b698f302c
Michael Sumner <mdsumner@gmail.com>
2014-10-21 22:03:45 +1100

    literal lib paths for root

--- RCMIP5_NetCDF4.sh
+++ RCMIP5_NetCDF4.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 ## 1) Update and install R
 
+## optional set your timezone
+rm /etc/localtime
+ln -s /usr/share/zoneinfo/Etc/GMT+10 /etc/localtime
+
 ## using Nectar image "NeCTAR Ubuntu 14.04 (Trusty) amd64"
 ## key for apt-get update, see http://cran.r-project.org/bin/linux/ubuntu/README
 ## chown ubuntu /etc/apt/sources.list
 echo 'deb http://cran.csiro.au/bin/linux/ubuntu trusty/' >> /etc/apt/sources.list
+echo 'local({ r <- getOption("repos"); r["CRAN"] <- "http://cran.csiro.au/"; options(repos = r); })' >> /etc/R/Rprofile.site
+
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E084DAB9
 apt-get update --assume-yes
 apt-get upgrade --assume-yes
@@ -16,7 +22,7 @@ apt-get install libnetcdf-dev --assume-yes
 
 ## 2) Install R package dependencies
 ## lib = "/usr/lib/R/site-library"
-Rscript -e 'x <- .libPaths();install.packages(c("abind", "plyr", "reshape2", "foreach", "digest", "ggplot2", "ncdf4", "ncdf", "doParallel", "fields", "testthat", "knitr"), lib = x[length(x)-1], repos = "http://cran.csiro.au")'
+Rscript -e 'install.packages(c("abind", "plyr", "reshape2", "foreach", "digest", "ggplot2", "ncdf4", "ncdf", "doParallel", "fields", "testthat", "knitr"), lib = "/usr/lib/R/site-library", repos = "http://cran.csiro.au")'
 
 ## 3) Install RCMIP5
-Rscript -e 'x <- .libPaths();install.packages("RCMIP5", lib = x[length(x)-1], repos = "http://cran.csiro.au")'
+Rscript -e 'install.packages("RCMIP5", lib = "/usr/lib/R/site-library", repos = "http://cran.csiro.au")'
