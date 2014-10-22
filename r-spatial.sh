c139d48a5d97e01d5f6dcdc3761059b40923442e
Michael Sumner <mdsumner@gmail.com>
2014-10-22 04:36:20 -1000

    don't forget raster!

--- r-spatial.sh
+++ r-spatial.sh
@@ -8,13 +8,10 @@
 ##   otherwise they run as root in startup, or as sudo by normal bash script
 
 ## This script installs:
-## - R with rgdal, ncdf4, and rgeos
-## - including HDF4, HDF5, NetCDF-4 and OpenDAP capabilities
+## - R with rgdal, ncdf4, rgeos and raster
+## - including HDF4, HDF5, NetCDF-4 and OpenDAP capabilities via rgdal or raster
 ## - using Nectar image "NeCTAR Ubuntu 14.04 (Trusty) amd64"
 
-## local time Tasmania
-rm /etc/localtime
-ln -s /usr/share/zoneinfo/Etc/GMT+10 /etc/localtime
 
 ## key for apt-get update, see http://cran.r-project.org/bin/linux/ubuntu/README
 echo 'deb http://cran.csiro.au/bin/linux/ubuntu trusty/' >> /etc/apt/sources.list
@@ -23,6 +20,9 @@ apt-get update
 apt-get upgrade --assume-yes
 
 ## Install 3rd parties
+
+## pretty sure not 100% sure the -tools and -bin versions 
+## are not needed for R but they are useful
 
 ## HDF4
 apt-get install libhdf4-dev --assume-yes
@@ -35,8 +35,9 @@ apt-get install netcdf-bin --assume-yes
 ## PROJ.4 and GDAL
 apt-get install libproj --assume-yes
 apt-get install libgdal-dev --assume-yes
+apt-get install gdal-bin --assume-yes
 
 ## now R
 apt-get install r-base r-base-dev --assume-yes
 echo 'local({ r <- getOption("repos"); r["CRAN"] <- "http://cran.csiro.au/";options(repos = r); })' >> /etc/R/Rprofile.site
-Rscript -e 'install.packages(c("rgdal", "rgeos", ncdf4"), "/usr/lib/R/site-library")'
+Rscript -e 'install.packages(c("rgdal", "rgeos", "ncdf4", "raster"), "/usr/lib/R/site-library")'
