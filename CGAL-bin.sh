a1acd953f46f918c43a7da68e887cc5adc36672c
Michael Sumner <mdsumner@gmail.com>
2014-10-27 10:19:25 +1100

    QGLViewer dependency for demos

--- CGAL-bin.sh
+++ CGAL-bin.sh
@@ -13,5 +13,16 @@ apt-get install cmake g++
 tar --gzip -xf /usr/share/doc/libcgal-demo/examples.tar.gz
 tar --gzip -xf /usr/share/doc/libcgal-demo/demo.tar.gz
 
+## QGLViewer (also need GLEW for some Mesh_3 demos, not sure yet)
+wget http://www.libqglviewer.com/src/libQGLViewer-2.5.3.tar.gz
+
+tar -xzf libQGLViewer-2.5.3.tar.gz
+cd libQGLViewer-2.5.3/QGLViewer
+
+qmake
+make
+sudo make install
+########################################
+
 
 ## http://homepages.math.uic.edu/~ddumas/teaching/2014/spring/mcs481/cgal-example/
