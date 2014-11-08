5736a5da71d15e750daca6f985ce4462cf0788f5
mdsumner <mdsumner@gmail.com>
2014-11-08 13:20:40 +1100

    Create revo-swine.sh

---
+++ revo-swine.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+echo 'deb http://cran.csiro.au/bin/linux/ubuntu trusty/' >> /etc/apt/sources.list
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E084DAB9
+
+## updated GDAL 
+## https://launchpad.net/~ubuntugis/+archive/ubuntu/ubuntugis-unstable
+add-apt-repository ppa:ubuntugis/ubuntugis-unstable --yes
+
+apt-get update 
+apt-get upgrade --assume-yes
+
+## http://mran.revolutionanalytics.com/documents/rro/installation/#revorinst-lin
+wget http://mran.revolutionanalytics.com/install/RRO-8.0-Beta-Ubuntu-14.04.x86_64.tar.gz
+
+tar zxvf RRO-8.0-Beta-Ubuntu-14.04.x86_64.tar.gz
+
+gdebi RRO-8.0-Beta-Ubuntu-14.04.x86_64.deb
+
+apt-get install gdebi-core
+apt-get install libapparmor1 # Required only for Ubuntu, not Debian
+wget http://download2.rstudio.org/rstudio-server-0.98.1091-amd64.deb
+gdebi rstudio-server-0.98.1091-amd64.deb
+
