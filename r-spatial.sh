70e28255a326a42ffb39527cbe27f018f74860f0
mdsumner <mdsumner@gmail.com>
2014-11-10 11:39:00 +1100

    Update r-spatial.sh

--- r-spatial.sh
+++ r-spatial.sh
@@ -5,9 +5,6 @@
 
 ## Paste all this text into "Post-Creation/Configuration Script"" or run as `sudo r-spatial.sh`
 
-## - NOTE: commands may be run individually must be run as sudo for ubuntu account, 
-##   otherwise they run as root in startup, or as sudo by normal bash script
-
 ## This script installs:
 ## - R with rgdal, ncdf4, rgeos and raster
 ## - including HDF4, HDF5, NetCDF-4 and OpenDAP capabilities via rgdal or raster
@@ -38,23 +35,6 @@ apt-get install hdf4-tools --assume-yes
 ## NetCDF
 apt-get install libnetcdf-dev --assume-yes
 apt-get install netcdf-bin --assume-yes
-
-## optional ECW
-# wget http://downloada.erdas.com/software/2014/ECWJP2SDKSetup/v5.1.1.49_Linux/ECWJP2SDKSetup_5.1.1.bin
-# chmod +x ECWJP2SDKSetup_5.1.1.bin
-# ./ECWJP2SDKSetup_5.1.1.bin #Select desktop-read-only and accept the license)
-# sudo cp -r ERDAS-ECW_JPEG_2000_SDK-5.1.1 /usr/local/
-# sudo ln -s /usr/local/ERDAS-ECW_JPEG_2000_SDK-5.1.1/Desktop_Read-Only/lib/x64/release/libNCSEcw.so /usr/local/lib/libNCSEcw.so
-# sudo ldconfig
-# wget https://launchpad.net/~ubuntugis/+archive/ubuntugis-unstable/+files/libgdal-ecw-src_1.10.0-1~precise4_all.deb
-# ar vx libgdal-ecw-src_1.10.0-1~precise4_all.deb
-# tar -xzf data.tar.gz
-# sudo cp usr/src/libgdal-ecw-1.10.0.tar.gz /usr/src/
-# sudo cp    usr/bin/gdal-ecw-build /usr/bin/
-# sudo gdal-ecw-build /usr/local/ERDAS-ECW_JPEG_2000_SDK-5.1.1/Desktop_Read-Only
-# sudo mkdir /usr/lib/gdalplugins/1.11
-# cd /usr/lib/gdalplugins/1.10
-# cp gdal_ECW_JP2ECW.so /usr/lib/gdalplugins/1.11 
 
 
 ## PROJ.4 and GDAL
