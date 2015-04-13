85fd02c42f523c2df79ef925e2a5e68a0af98a2c
mdsumner <mdsumner@gmail.com>
2015-04-13 14:05:51 +1000

    Update docker_pg.sh

--- docker_pg.sh
+++ docker_pg.sh
@@ -7,6 +7,7 @@ docker pull postgres
 docker pull rocker/hadleyverse
 
 sudo docker run -d -p 8787:8787 rocker/hadleyverse
-
-docker run --name ocdb -e POSTGRES_PASSWORD=T4llH0ps -d postgres
+docker run --name ocdb -e POSTGRES_PASSWORD=secret -d postgres
+docker run -it --link ocdb:postgres --rm postgres sh -c 'exec psql -h "$POSTGRES_PORT_5432_TCP_ADDR" -p "$POSTGRES_PORT_5432_TCP_PORT" -U postgres'
+##docker run --name some-postgres -e POSTGRES_PASSWORD=mysecretpassword -d postgres
 sudo docker run -d -p 8787:8787 -P --name rstd --link ocdb:ocdb rocker/hadleyverse
