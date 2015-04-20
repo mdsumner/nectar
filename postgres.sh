2130514fcb56811f696e3955835cf680396f6e4e
mdsumner <mdsumner@gmail.com>
2015-04-20 21:36:02 +1000

    Update postgres.sh

--- postgres.sh
+++ postgres.sh
@@ -26,6 +26,7 @@ apt-get install hdf4-tools --assume-yes
 apt-get install libnetcdf-dev --assume-yes
 apt-get install netcdf-bin --assume-yes
 
+## PostgreSQL now so that GDAL and rgdal automatically pick it and PostGIS up
 ##http://wiki.openstreetmap.org/wiki/PostGIS/Installation
 apt-get install postgresql postgresql-contrib postgis postgresql-9.3-postgis-2.1
 ## https://www.digitalocean.com/community/tutorials/how-to-install-and-use-postgresql-on-ubuntu-14-04
@@ -39,37 +40,16 @@ apt-get install gdal-bin --assume-yes
 ## now R
 apt-get install r-base r-base-dev --assume-yes
 echo 'local({ r <- getOption("repos"); r["CRAN"] <- "http://cran.csiro.au/";options(repos = r); })' >> /etc/R/Rprofile.site
-Rscript -e 'install.packages(c("rgdal", "rgeos", "ncdf4", "raster"), "/usr/local/lib/R/site-library")'
+Rscript -e 'install.packages(c("rgdal", "rgeos", "ncdf4", "raster", "dplyr", "RPostgreSQL"), "/usr/local/lib/R/site-library")'
+
+## create normal user etc. 
+#sudo adduser azif
+##sudo -i -u postgres
+##createdb azif
+##createuser --interactive
 
 
-sudo adduser mdsumner
-sudo -i -u postgres
-createdb mdsumner
-createuser --interactive
-
-##SHOW data_directory;
-## /var/lib/postgresql/9.3/main
-
-/usr/local/pgsql/data
-
-## attach volume  to 
-sudo apt-get install gdebi-core --assume-yes
-sudo apt-get install libapparmor1 --assume-yes
-
+## consider (for PostGIS)
 # CGAL
 sudo apt-get install libcgal-dev
 sudo apt-get install libcgal-demo
-
-sudo apt-get install postgresql postgresql-contrib postgresql-devel 
-sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt trusty-pgdg main" >> /etc/apt/sources.list'
-wget --quiet -O - http://apt.postgresql.org/pub/repos/apt/ACCC4CF8.asc | sudo apt-key add -
-sudo apt-get update
-sudo apt-get install postgresql-9.4-postgis-2.1 pgadmin3 ##postgresql-contrib
-sudo apt-get postgresql-client-common postgres-xc-client
-
-#sudo su - postgres psql
-#create user  mdsumner with password 'hoochymama';
-#create database ocdb;
-#grant all privileges on database ocdb to mdsumner;
-
-Rscript -e 'install.packages(c("dplyr", "RPostgreSQL"), "/usr/local/lib/R/site-library")'
