a719631a7588702b29d1901005afabc164f15a0c
mdsumner <mdsumner@gmail.com>
2015-04-21 06:35:58 +1000

    Update example.R

--- PostgreSQL/example.R
+++ PostgreSQL/example.R
@@ -9,14 +9,15 @@
 ## this works, need to improve
 library(dplyr)
 load("massivetable.Rdata")
-x <- x %.% mutate(year = as.integer(substr(filename, 2, 5)), jday = as.integer(substr(filename, 6, 8)))
+x <- x %>% mutate(year = as.integer(substr(filename, 2, 5)), jday = as.integer(substr(filename, 6, 8)))
 my_db <- src_postgres()
 con <- dbConnect(PostgreSQL())
 
-chunk <- 1000
+chunk <- 100000
 cnt <- seq(1, chunk)
- dbiWriteTable(con, x[cnt, ], "oc_db")
+ dbWriteTable(my_db, as.data.frame(x[cnt, ]), "oc_db")
 cnt <- cnt + chunk
+
 while(TRUE) {
    dbWriteTable(con, "oc_db", as.data.frame(x[cnt, ]), append = TRUE)
    cnt <- cnt + chunk
@@ -27,3 +28,4 @@ my_db <- src_postgres()
 d <- tbl(my_db, "oc_db")
 ##xi <- db_create_index(my_db, "oc_db", list(c("year", "jday"), "bin_num"), name = NULL, ...)
 
+CREATE INDEX test2_mm_idx ON oc_db ((year, jday), bin_num);
