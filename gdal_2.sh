3eda9465a6d6132889c7d1fbf6c4597e00bda351
mdsumner <mdsumner@gmail.com>
2015-06-03 00:53:55 +1000

    Update gdal_2.sh

--- gdal_2.sh
+++ gdal_2.sh
@@ -1,13 +1,19 @@
 sudo apt-get update
-sudo apt-get upgrade
-sudo apt-get install git
-git clone https://github.com/OSGeo/gdal.git
+sudo apt-get upgrade --assume-yes
+sudo apt-get install git --assume-yes
+sudo apt-get install libnetcdf-dev --assume-yes
+sudo apt-get install netcdf-bin --assume-yes
 sudo apt-get install libhdf4-dev --assume-yes
-sudo apt-get install build-essential
-sudo apt-get install libproj-dev
+sudo apt-get install build-essential --assume-yes
+sudo apt-get install libproj-dev --assume-yes
+sudo apt-get install proj-bin --assume-yes
+git clone https://github.com/OSGeo/gdal.git
+
 cd gdal
 ./configure
 make 
 make install
 export LD_LIBRARY_PATH=/usr/local/lib
+
+
 
