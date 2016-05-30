a81c0fe36b0d55a467345a8c536ad6c5107f972b
Michael Sumner <mdsumner@gmail.com>
2016-05-30 17:42:43 +1000

    Update r-debian.sh

--- r-debian.sh
+++ r-debian.sh
@@ -1,27 +1,14 @@
 #!/bin/bash
 
-## Install R and geo-spatial dependencies
-##  (a bit of a moving feast, let me know if you need help)
+## sources.list
+#deb http://ftp.au.debian.org/debian  sid main
+#deb-src http://ftp.au.debian.org/debian sid main
 
-
-## Paste all this text into "Post-Creation/Configuration Script"" or run as `sudo r-spatial.sh`
-
-## This script installs:
-## - R with rgdal, ncdf4, rgeos and raster
-## - including HDF4, HDF5, NetCDF-4 and OpenDAP capabilities via rgdal or raster
-## - using Nectar image "NeCTAR Ubuntu 14.04 (Trusty) amd64"
-## - see final comments for RStudio Server, which should be done interactively - and update the
-## version by checking the website noted for "wget" and subsequent "gdebi" install
 
 apt-get install apt-transport-https --assume-yes
 
-## key for apt-get update, see http://cran.r-project.org/bin/linux/ubuntu/README
-echo 'deb https://cloud.r-project.org/bin/linux/debian jessie-cran3/' >> /etc/apt/sources.list
-apt-key adv --keyserver keys.gnupg.net --recv-key 381BA480
 
-echo "deb http://http.debian.net/debian jessie main" > /etc/apt/sources.list.d/debian-unstable.list \
-	&& echo 'APT::Default-Release "testing";' > /etc/apt/apt.conf.d/default
-
+	
 apt-get update 	
 apt-get upgrade --assume-yes
 
@@ -44,38 +31,26 @@ apt-get install libgdal-dev --assume-yes
 apt-get install gdal-bin --assume-yes
 apt-get install libgeos-dev --assume-yes
 
-apt-get install libssl-dev  --assume-yes
+ apt-get install libudunits2-0 libudunits2-dev
+
+##apt-get install libssl-dev  --assume-yes
+
+apt-get install jags --assume-yes
 
 ## now R
 apt-get install r-base r-base-dev --assume-yes
-echo 'local({ r <- getOption("repos"); r["CRAN"] <- "http://cran.csiro.au/";options(repos = r); })' >> /etc/R/Rprofile.site
-Rscript -e 'install.packages(c("devtools", "rgdal", "rgeos", "ncdf4", "raster"), "/usr/local/lib/R/site-library")'
+echo 'local({ r <- getOption("repos"); r["CRAN"] <- "https://cloud.r-project.org/";options(repos = r); })' >> /etc/R/Rprofile.site
+Rscript -e 'install.packages(c("devtools", "rjags", "PBSmapping", "RNetCDF", "rgdal", "rgeos", "ncdf4", "raster"), "/usr/local/lib/R/site-library")'
 
-## LaTeX, pandoc etc. nightmare - just google rstudio errors and follow the trail . . .
-## sudo apt-get install ec
-
-#apt-get install texlive-fonts-recommended --assume-yes
-#apt-get install texlive-latex-recommended --assume-yes
 
 apt-get install gdebi-core --assume-yes
-apt-get install libapparmor1 --assume-yes # Required only for Ubuntu, not Debian
+
 ## uncomment and run gdebi with sudo
-##wget http://download2.rstudio.org/rstudio-server-0.98.1091-amd64.deb
-##gdebi rstudio-server-0.98.1091-amd64.deb --assume-yes
-
-## RStudio server?, uncomment and run interactively
-## see here for updates: http://www.rstudio.com/products/rstudio/download-server/
-# sudo apt-get install gdebi-core --assume-yes
-## already installed above by something, but no harm
-# sudo apt-get install libapparmor1 --assume-yes # Required only for Ubuntu, not Debian
-#  wget http://download2.rstudio.org/rstudio-server-0.98.1085-amd64.deb
-# sudo gdebi rstudio-server-0.98.1085-amd64.deb
-
-##wget http://download3.rstudio.org/ubuntu-12.04/x86_64/shiny-server-1.2.3.368-amd64.deb
-## sudo gdebi shiny-server-1.2.3.368-amd64.deb
+##wget https://s3.amazonaws.com/rstudio-dailybuilds/rstudio-0.99.1200-amd64.deb
+##gdebi rstudio-0.99.1200-amd64.deb
 
 
 ## rgl
 ## 2016-03-10
-apt-get install libgl1-mesa-dev libglu1-mesa-dev
+#apt-get install libgl1-mesa-dev libglu1-mesa-dev
 
