d9e4f35245d4464005d21f352e7006c0b1a1d6dc
Michael Sumner <mdsumner@gmail.com>
2016-06-01 16:03:03 +1000

    Create routine_install.r

---
+++ routine_install.r
@@ -0,0 +1,14 @@
+install.packages(c("devtools", "rjags", "PBSmapping", "RNetCDF", "rgdal", "rgeos", "ncdf4", "raster", "RODBC", "shiny", "DT", "readr", "readxl", "dygraphs")
+
+devtools::install_github("ianjonsen/bsam", ref = "dev-new")
+devtools::install_github("environmentalinformatics-marburg/mapview", ref = "develop")
+devtools::install_github("rstudio/bookdown")
+
+devtools::install_github("AustralianAntarcticDivision/raadtools")
+devtools::install_github("AustralianAntarcticDivision/palr")
+devtools::install_github("AustralianAntarcticDivision/rbgm")
+devtools::install_github("mdsumner/graticule")
+devtools::install_github("mdsumner/trip")
+devtools::install_github("mdsumner/spbabel")
+devtools::install_github("mdsumner/spdplyr")
+devtools::install_github("mdsumner/orsifronts")
