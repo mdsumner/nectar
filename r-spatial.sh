ae96b00e7e80c94173163772a7046ae084964cec
Michael Sumner <mdsumner@gmail.com>
2017-05-22 18:16:01 +1000

    Update r-spatial.sh

--- r-spatial.sh
+++ r-spatial.sh
@@ -11,28 +11,21 @@ apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E084DAB9
 ## https://launchpad.net/~ubuntugis/+archive/ubuntu/ubuntugis-unstable
 add-apt-repository ppa:ubuntugis/ubuntugis-unstable --yes
 
-apt-get update 
-apt-get upgrade --assume-yes
+apt update 
+apt upgrade --assume-yes
 
 ## Install 3rd parties
 
 ## NetCDF
-apt-get install libnetcdf-dev --assume-yes && \
-
-## PROJ.4 and GDAL and R
-apt-get install proj-bin --assume-yes && \
-apt-get install libproj-dev --assume-yes && \
-apt-get install libgdal-dev --assume-yes && \
-apt-get install gdal-bin --assume-yes && \
-apt-get install libgeos-dev --assume-yes && \
-apt-get install libssl-dev  --assume-yes && \
-apt-get install libudunits2-dev --assume-yes && \
-apt-get install r-base r-base-dev --assume-yes
+apt install libnetcdf-dev proj-bin  libproj-dev libgdal-dev gdal-bin libgeos-dev  libssl-dev   libudunits2-dev  r-base r-base-dev --assume-yes
+   
 echo 'local({ r <- getOption("repos"); r["CRAN"] <- "https://mirror.aarnet.edu.au/pub/CRAN/";options(repos = r); })' >> /etc/R/Rprofile.site
-Rscript -e 'install.packages(c("devtools", "rgdal", "rgeos", "ncdf4", "raster", "sf", "tibble", "tidyverse", "rworldmap", "graticule"), "/usr/local/lib/R/site-library")'
-Rscript -e 'install.packages(c("backports", "base64enc", "bitops", "caTools", "crayon", "evaluate",  "formatR",  "highr", "htmltools", "htmlwidgets", "httpuv", "knitr",  "markdown", "praise", "proj4", "rmarkdown", "rprojroot",   "shiny", "sourcetools",  "testthat", "xtable", "yaml"), "/usr/local/lib/R/site-library")'
+Rscript -e 'install.packages(c("devtools", "rgdal", "rgeos", "ncdf4", "raster", "sf", "tibble", "tidyverse", "rworldmap"), "/usr/local/lib/R/site-library")'
+Rscript -e 'install.packages(c("backports", "base64enc", "bitops", "caTools", "crayon", "evaluate",  "formatR",  "ggraph", "ggforce", "highr", "htmltools", "htmlwidgets", "httpuv", "knitr",  "markdown", "praise", "proj4", "rmarkdown", "rprojroot",   "shiny", "sourcetools",  "testthat", "xtable", "yaml", "angstroms", "graticule", "ncdump", "rbgm", "spdplyr", "spbabel", "spex"), "/usr/local/lib/R/site-library")
+Rscript -e 'devtools::install_github(c("rstudio/leaflet", "bhaskarvk/leaflet.extras", "environmentalinformatics-marburg/mapview@develop"), "/usr/local/lib/R/site-library")
+
 apt-get install gdebi-core --assume-yes
-wget https://s3.amazonaws.com/rstudio-dailybuilds/rstudio-1.1.96-amd64.deb
-gdebi rstudio-dailybuilds/rstudio-1.1.96-amd64.deb -n
+wget https://s3.amazonaws.com/rstudio-dailybuilds/rstudio-server-1.1.238-amd64.deb
+gdebi rstudio-server-1.1.238-amd64.deb -n
 
 
