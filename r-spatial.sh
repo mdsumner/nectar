6e55ac30753ea2f3f89b6e6d757549bec1066c86
Michael Sumner <mdsumner@gmail.com>
2017-06-14 12:26:41 +1000

    Create r-spatial.sh

--- r-spatial.sh
+++ r-spatial.sh
@@ -4,7 +4,7 @@
 ##  (a bit of a moving feast, let me know if you need help)
 
 ## key for apt-get update, see http://cran.r-project.org/bin/linux/ubuntu/README
-echo 'deb https://mirror.aarnet.edu.au/pub/CRAN/bin/linux/ubuntu xenial/' >> /etc/apt/sources.list
+echo 'deb https://cran.csiro.au/bin/linux/ubuntu xenial/' >> /etc/apt/sources.list
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E084DAB9
 
 ## updated GDAL 
@@ -16,16 +16,23 @@ apt upgrade --assume-yes
 
 ## Install 3rd parties
 
-## NetCDF
+## NetCDF and geo-spatial wunderkind
 apt install libnetcdf-dev proj-bin  libproj-dev libgdal-dev gdal-bin libgeos-dev  libssl-dev   libudunits2-dev  r-base r-base-dev --assume-yes
    
-echo 'local({ r <- getOption("repos"); r["CRAN"] <- "https://mirror.aarnet.edu.au/pub/CRAN/";options(repos = r); })' >> /etc/R/Rprofile.site
+# R packages
+echo 'local({ r <- getOption("repos"); r["CRAN"] <- "https://cran.csiro.au/";options(repos = r); })' >> /etc/R/Rprofile.site
 Rscript -e 'install.packages(c("devtools", "rgdal", "rgeos", "ncdf4", "raster", "RNetCDF", "sf", "tibble", "tidyverse", "rworldmap"), "/usr/local/lib/R/site-library")'
-Rscript -e 'install.packages(c("backports", "base64enc", "bitops", "caTools", "crayon", "evaluate",  "formatR",  "ggraph", "ggforce", "highr", "htmltools", "htmlwidgets", "httpuv", "knitr",  "markdown", "praise", "proj4", "rmarkdown", "rprojroot",   "shiny", "sourcetools",  "testthat", "xtable", "yaml", "angstroms", "graticule", "ncdump", "rbgm", "spdplyr", "spbabel", "spex"), "/usr/local/lib/R/site-library")
-Rscript -e 'devtools::install_github(c("rstudio/leaflet", "bhaskarvk/leaflet.extras", "environmentalinformatics-marburg/mapview@develop"), "/usr/local/lib/R/site-library")
+#Rscript -e 'install.packages(c("backports", "base64enc", "bitops", "caTools", "crayon", "evaluate",  "formatR",  "ggraph", "ggforce", "highr", "htmltools", "htmlwidgets", "httpuv", "knitr",  "markdown", "praise", "proj4", "rmarkdown", "rprojroot",   "shiny", "sourcetools",  "testthat", "xtable", "yaml", "angstroms", "graticule", "ncdump", "rbgm", "spdplyr", "spbabel", "spex"), "/usr/local/lib/R/site-library")
+#Rscript -e 'devtools::install_github(c("rstudio/leaflet", "bhaskarvk/leaflet.extras", "environmentalinformatics-marburg/mapview@develop"), "/usr/local/lib/R/site-library")
 
+## RSTudio
 apt-get install gdebi-core --assume-yes
-wget https://s3.amazonaws.com/rstudio-dailybuilds/rstudio-server-1.1.238-amd64.deb
-gdebi rstudio-server-1.1.238-amd64.deb -n
+## find daily builds here
+#wget https://s3.amazonaws.com/rstudio-dailybuilds/rstudio-server-1.1.238-amd64.deb
+wget https://download2.rstudio.org/rstudio-server-1.0.143-amd64.deb
+gdebi rstudio-server-1.0.143-amd64.deb -n
 
-
+## then, regularly
+# apt update
+# apt upgrade
+# Rscript -e 'devtools::update_packages(TRUE)'
