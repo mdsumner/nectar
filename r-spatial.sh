28115b78a447253d5aa6ace1cdcdc53ab43668eb
Michael Sumner <mdsumner@gmail.com>
2017-09-01 14:46:13 +1000

    rgl

--- r-spatial.sh
+++ r-spatial.sh
@@ -17,20 +17,18 @@ apt upgrade --assume-yes
 ## Install 3rd parties
 
 ## NetCDF and geo-spatial wunderkind
-apt install libarchive-dev libnetcdf-dev proj-bin  libproj-dev libgdal-dev gdal-bin libgeos-dev  libssl-dev   libudunits2-dev libprotobuf-dev protobuf-compiler imagemagick r-base r-base-dev --assume-yes
+apt install gdebi-core libarchive-dev libnetcdf-dev proj-bin  libproj-dev libgdal-dev gdal-bin libgeos-dev  libssl-dev  libgl1-mesa-dev libudunits2-dev libprotobuf-dev protobuf-compiler imagemagick r-base r-base-dev --assume-yes
    
 # R packages
 echo 'local({ r <- getOption("repos"); r["CRAN"] <- "https://cran.csiro.au/";options(repos = r); })' >> /etc/R/Rprofile.site
-Rscript -e 'install.packages(c("devtools", "rgdal", "rgeos", "ncdf4", "raster", "RNetCDF", "sf", "tibble", "tidyverse", "rworldmap", "geojsonio", "magick"), "/usr/local/lib/R/site-library")'
+Rscript -e 'install.packages(c("devtools", "sfdct", "rgdal", "leaflet",  "rgeos", "ncdf4", "raster", "RNetCDF", "sf", "tibble", "tidyverse", "rworldmap", "geojsonio", "magick", "rgl", "tabularaster", "angstroms", "spex", "graticule"), "/usr/local/lib/R/site-library")'
 #Rscript -e 'install.packages(c("backports", "base64enc", "bitops", "caTools", "crayon", "evaluate",  "formatR",  "ggraph", "ggforce", "highr", "htmltools", "htmlwidgets", "httpuv", "knitr",  "markdown", "praise", "proj4", "rmarkdown", "rprojroot",   "shiny", "sourcetools",  "testthat", "xtable", "yaml", "angstroms", "graticule", "ncdump", "rbgm", "spdplyr", "spbabel", "spex"), "/usr/local/lib/R/site-library")
 #Rscript -e 'devtools::install_github(c("rstudio/leaflet", "bhaskarvk/leaflet.extras", "environmentalinformatics-marburg/mapview@develop"), "/usr/local/lib/R/site-library")
 
-## RSTudio
-apt-get install gdebi-core --assume-yes
 ## find daily builds here
-#wget https://s3.amazonaws.com/rstudio-dailybuilds/rstudio-server-1.1.238-amd64.deb
-wget https://download2.rstudio.org/rstudio-server-1.0.143-amd64.deb
-gdebi rstudio-server-1.0.143-amd64.deb -n
+#wget https://s3.amazonaws.com/rstudio-dailybuilds
+wget https://s3.amazonaws.com/rstudio-dailybuilds/rstudio-server-1.1.353-amd64.deb
+gdebi rstudio-server-1.1.353-amd64.deb -n
 
 ## then, regularly
 # apt update
