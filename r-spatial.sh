cfe88cae381b1f5cf7b0f890f594321e2bd8c5ec
Michael Sumner <mdsumner@gmail.com>
2017-10-11 18:10:57 +1100

    Update r-spatial.sh

--- r-spatial.sh
+++ r-spatial.sh
@@ -11,19 +11,22 @@ apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E084DAB9
 ## https://launchpad.net/~ubuntugis/+archive/ubuntu/ubuntugis-unstable
 add-apt-repository ppa:ubuntugis/ubuntugis-unstable --yes
 
+## opencpu (for geojson)
+add-apt-repository -y ppa:opencpu/jq
+
 apt update 
 apt upgrade --assume-yes
 
 ## Install 3rd parties
 
 ## NetCDF and geo-spatial wunderkind
-apt install gdebi-core libv8-3.14-dev  libmagick++-dev libarchive-dev libnetcdf-dev proj-bin  libproj-dev libgdal-dev gdal-bin libgeos-dev  libssl-dev libgl1-mesa-dev libglu1-mesa-dev libudunits2-dev libprotobuf-dev protobuf-compiler imagemagick r-base r-base-dev --assume-yes
+apt install libjq-dev gdebi-core libv8-3.14-dev  libmagick++-dev libarchive-dev libnetcdf-dev proj-bin  libproj-dev libgdal-dev gdal-bin libgeos-dev  libssl-dev libgl1-mesa-dev libglu1-mesa-dev libudunits2-dev libprotobuf-dev protobuf-compiler imagemagick r-base r-base-dev --assume-yes
    
 # R packages
 echo 'local({ r <- getOption("repos"); r["CRAN"] <- "https://cran.csiro.au/";options(repos = r); })' >> /etc/R/Rprofile.site
 Rscript -e 'install.packages(c("devtools", "sfdct", "rgdal", "leaflet",  "rgeos", "ncdf4", "raster", "RNetCDF", "sf", "tibble", "tidyverse", "rworldmap", "geojsonio", "magick", "rgl", "tabularaster", "angstroms", "spex", "graticule"), "/usr/local/lib/R/site-library")'
-Rscript -e 'install.packages(c("backports", "base64enc", "bitops", "caTools", "crayon", "evaluate",  "formatR",  "ggraph", "ggforce", "highr", "htmltools", "htmlwidgets", "httpuv", "knitr",  "markdown", "praise", "proj4", "rmarkdown", "rprojroot",   "shiny", "sourcetools",  "testthat", "xtable", "yaml", "angstroms", "graticule", "ncdump", "rbgm", "spdplyr", "spbabel", "spex"), "/usr/local/lib/R/site-library")
-Rscript -e 'devtools::install_github(c("bhaskarvk/leaflet.extras", "environmentalinformatics-marburg/mapview@develop", "SWotherspoon/SGAT", "SWotherspoon/BAStag", "hypertidy/tidync"), "/usr/local/lib/R/site-library")
+Rscript -e 'install.packages(c("backports", "base64enc", "bitops", "caTools", "crayon", "evaluate",  "formatR",  "ggraph", "ggforce", "highr", "htmltools", "htmlwidgets", "httpuv", "knitr",  "markdown", "praise", "proj4", "rmarkdown", "rprojroot",   "shiny", "sourcetools",  "testthat", "xtable", "yaml", "angstroms", "graticule", "ncdump", "rbgm", "spdplyr", "spbabel", "spex"), "/usr/local/lib/R/site-library")'
+Rscript -e 'devtools::install_github(c("bhaskarvk/leaflet.extras", "environmentalinformatics-marburg/mapview@develop", "SWotherspoon/SGAT", "SWotherspoon/BAStag", "hypertidy/tidync"), "/usr/local/lib/R/site-library")'
 
 ## find daily builds here
 #wget https://s3.amazonaws.com/rstudio-dailybuilds
