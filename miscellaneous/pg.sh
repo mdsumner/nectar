ed3618fe4766443067b563f912cc4d462e8be2a1
Michael Sumner <mdsumner@gmail.com>
2022-04-15 17:08:49 +1000

    Update pg.sh

--- miscellaneous/pg.sh
+++ miscellaneous/pg.sh
@@ -5,8 +5,10 @@ sudo apt upgrade
 sudo apt -y install gnupg2
 wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
 
+sudo add-apt-repository ppa:ubuntugis/ubuntugis-unstable --yes
+echo "deb http://apt.postgresql.org/pub/repos/apt/ `lsb_release -cs`-pgdg main" | sudo tee /etc/apt/sources.list.d/pgdg.list
 
-sudo apt install postgis postgresql-13-postgis-3
+sudo apt install postgis postgresql 
 
 sudo -i -u postgres
 
@@ -16,5 +18,4 @@ createdb postgis_db -O postgis_test
 
 psql -d postgis_db
 
-sudo apt update
 CREATE EXTENSION postgis;
